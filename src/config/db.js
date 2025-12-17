0359c5432a83235e4af7ebdcd376f3e77286b04b
MasadRayan <masadrayan2002@gmail.com>
2025-12-17 22:14:40 +0600

    Database Connection built

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MongoDB_URI
+
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function connectDB() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (e) {
+    console.error("DB Conenction Failed",e);
+  }
+}
