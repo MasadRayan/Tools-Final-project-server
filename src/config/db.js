a5d17146830116d68f5d04e118d3ebbe9c201d2b
MasadRayan <masadrayan2002@gmail.com>
2025-12-17 22:29:29 +0600

    Finally connected the server to the database

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.MongoDB_URI
 
 export const client = new MongoClient(uri, {
@@ -13,7 +13,7 @@ export async function connectDB() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("✅✅ Pinged your deployment. You successfully connected to MongoDB!");
   } catch (e) {
     console.error("DB Conenction Failed",e);
   }
