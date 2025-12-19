fa3f31c9f11b5290d7029875d78de446bbfef2d9
MasadRayan <masadrayan2002@gmail.com>
2025-12-20 00:16:48 +0600

    Paginated Products added done

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createProducts, getAllProducts, addManyProduct } from "../controllers/product.controller.js";
+import { createProducts, getAllProducts, addManyProduct, paginatedProducts } from "../controllers/product.controller.js";
 const productRouter = express.Router();
 
 productRouter.post("/", createProducts);
 productRouter.get("/", getAllProducts);
 productRouter.post("/many", addManyProduct);
-
+productRouter.get("/products-paginated", paginatedProducts)
 
 export default productRouter;
