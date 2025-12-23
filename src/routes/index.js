3344ace0156ca3340d69471f1145c478f876dd30
MasadRayan <masadrayan2002@gmail.com>
2025-12-23 20:28:27 +0600

    renaming the router name for ssl payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import userRouter from "./user.route.js";
 import productRouter from "./product.route.js";
-import paymentTouter from "./ssl-payment.js";
+import paymentTouter from "./ssl-payment.route.js";
 const router = express.Router();
 
 router.use("/users", userRouter);
