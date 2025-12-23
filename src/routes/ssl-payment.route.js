337bec1cb1a82c56aa893a78f0f7ceaae8266331
MasadRayan <masadrayan2002@gmail.com>
2025-12-24 04:53:58 +0600

    Admin verification added in the all payment route

--- src/routes/ssl-payment.route.js
+++ src/routes/ssl-payment.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createSSLPayment, getAllPayments, getUserPayments, sslPaymentSuccess } from "../controllers/ssl-payment.controller.js";
 import { verifyFBToken } from "../middlewares/verifyFBToken.js";
+import { verifyAdmin } from "../middlewares/verifyAdmin.js";
 
 const paymentTouter = express.Router();
 
 paymentTouter.post("/", verifyFBToken, createSSLPayment);
 paymentTouter.post("/success-payment", sslPaymentSuccess);
 paymentTouter.get("/user/:email", verifyFBToken, getUserPayments);
-paymentTouter.get("/allPayment", getAllPayments)
+paymentTouter.get("/allPayment", verifyFBToken, verifyAdmin, getAllPayments)
 export default paymentTouter;
