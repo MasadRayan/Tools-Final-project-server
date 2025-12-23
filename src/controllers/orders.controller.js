076f1c4bd0356119a440daab4fda8ac34fff45c3
MasadRayan <masadrayan2002@gmail.com>
2025-12-24 01:10:31 +0600

    Order router added

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -6,7 +6,7 @@ import { ObjectId } from "mongodb";
 export const createOrder = async (req, res) => {
     try {
         const order = req.body;
-        const {transactionID} = order;
+        const { transactionID } = order;
 
         // Validate that transactionID is provided
         if (!transactionID) {
@@ -21,7 +21,7 @@ export const createOrder = async (req, res) => {
 
         const result = await ordersCollection.insertOne(order);
         res.send(result);
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: "Internal server error" });
@@ -31,7 +31,24 @@ export const createOrder = async (req, res) => {
 // get all orders
 export const getAllOrders = async (req, res) => {
     try {
-        const result = await ordersCollection.find().toArray();
+        const page = parseInt(req.query.page) || 0;
+        const limit = 10;
+        const skip = page * limit;
+
+        const orders = await ordersCollection.find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+
+        const total = await ordersCollection.estimatedDocumentCount();
+
+        res.send({
+            total,
+            page,
+            limit,
+            data: orders
+        });
+
         res.send(result);
     } catch (error) {
         res.status(500).send({ message: "Internal server error" });
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createOrder } from "../controllers/orders.controller.js";
+import { createOrder, getAllOrders } from "../controllers/orders.controller.js";
 import { verifyFBToken } from "../middlewares/verifyFBToken.js";
+import { verifyAdmin } from "../middlewares/verifyAdmin.js";
 const orderRouter = express.Router();
 
 orderRouter.post("/", verifyFBToken, createOrder)
+orderRouter.get('/paginated-orders', verifyFBToken, verifyAdmin, getAllOrders);
 
 export default orderRouter;
