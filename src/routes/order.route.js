c9b7025fcf6a83fe3580f413c6ce8c03ba9e6f5e
MasadRayan <masadrayan2002@gmail.com>
2025-12-24 02:37:38 +0600

    Order route added

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createOrder, getAllOrders } from "../controllers/orders.controller.js";
+import { createOrder, getAllOrders, updateOrderStatus } from "../controllers/orders.controller.js";
 import { verifyFBToken } from "../middlewares/verifyFBToken.js";
 import { verifyAdmin } from "../middlewares/verifyAdmin.js";
 const orderRouter = express.Router();
 
 orderRouter.post("/", verifyFBToken, createOrder)
 orderRouter.get('/paginated-orders', verifyFBToken, verifyAdmin, getAllOrders);
+orderRouter.patch('/status/:id', verifyFBToken, verifyAdmin, updateOrderStatus)
 
 export default orderRouter;
